5f00fa78a508c78954de9965a2a2cc11f66c4628
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-07-17 19:19:15 -0300

    Exercício 4

---
+++ semana3/projetinho/projeto.js
@@ -0,0 +1,41 @@
+var convidado1 = {
+    nome: "Ana",
+    sobrenome: "Negri",
+    setor: "Camarote",
+    idade: 11,
+  };
+
+var convidado2 = {
+    nome: "Duda",
+    sobrenome: "Duarte",
+    setor: "Camarote",
+    idade: 18,
+  };
+
+var convidado3 = {
+    nome: "Clara",
+    sobrenome: "Alves",
+    setor: "Pista",
+    idade: 16,
+};
+
+var convidado4 = {
+    nome: "Talita",
+    sobrenome: "Alves",
+    setor: "Pista",
+    idade: 29,
+};
+
+var convidado5 = {
+    nome: "Caio",
+    sobrenome: "Souza",
+    setor: "Arquibancada",
+    idade: 26,
+};
+
+var convidado6 = {
+    nome: "Aline",
+    sobrenome: "Cardoso",
+    setor: "Arquibancada",
+    idade: 21,
+};
