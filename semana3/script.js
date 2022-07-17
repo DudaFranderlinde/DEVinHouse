54cee3b9cdb3830122124033c5a36d54f22f5353
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-07-17 16:13:07 -0300

    Exercício 2

--- semana3/script.js
+++ semana3/script.js
@@ -5,3 +5,28 @@ var listaNumeros = [ 2, 3, 9, 12, 16, 55, 90, 78, 17 ]
  })
 
  console.log(listaNumeros)
+
+// Exercício 2
+
+var listaConvidados = [
+    { nome: "José", sobrenome: "Carlos" },
+    { nome: "Alessandro", sobrenome: "Viana" },
+    { nome: "Paula", sobrenome: "Souza" },
+    { nome: "Cristian", sobrenome: "Schimit" },
+    { nome: "Beatriz", sobrenome: "Viana" },
+    { nome: "Fernanda", sobrenome: "Silveira" },
+    { nome: "Cláudia", sobrenome: "Torres" },
+    { nome: "Augusto", sobrenome: "Cesar" },
+    { nome: "Noemi", sobrenome: "Nakamura" },
+    { nome: "Pedro", sobrenome: "Lobo" },
+  ];
+ var convidados = document.getElementById('lista-convidados');
+ 
+  const listCompleta = listaConvidados.map((c,i)=>{
+  var nome = listaConvidados[i].nome +" "+ listaConvidados[i].sobrenome;
+  var item = document.createElement("li");
+  item.textContent = nome;
+  convidados.appendChild(item);
+ 
+   
+});
