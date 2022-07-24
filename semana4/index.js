fcffa0cc00b0fa6a06fe9ca858887eb4fbf6b417
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-07-24 15:06:21 -0300

    Exercício 4

--- semana4/index.js
+++ semana4/index.js
@@ -25,7 +25,6 @@ function addSelect(){
     const option = document.createElement('option');
     option.value = '0';
     option.textContent = '(Selecione)';
-
     selectConta.appendChild(option)
 
     contaCliente.forEach((contaCLi)=> {
@@ -38,3 +37,51 @@ function addSelect(){
 };
 
 body.onload = addSelect();
+
+function validarNumber(valor){
+    if(isNaN(valor) || valor <=0){
+        return false;
+    }
+
+    return true;
+}
+
+function validarSaldo(valor, saldo){
+    if(valor > saldo){
+        return false;
+    }
+
+    return true;
+}
+
+
+function atualizaSaldo(id_cliente, saldo){
+    const contasSemContaAtual = contaCliente.filter((c) => c.id !== id_cliente.id);
+    const contaAtualizada = { ...id_cliente, saldo };
+    const contasAtualizadas = [...contasSemContaAtual, contaAtualizada];
+    contaCliente = contasAtualizadas;
+console.log(contaCliente)
+
+}
+
+
+function sacar(contaCliente, valor){
+    let p = document.getElementById('p');
+    if(!validarNumber(valor)){
+        p.textContent = "Valor inválido. Tente novamente!";
+        console.log(" valor invalido");
+        return;
+    }
+    if(!validarSaldo(valor, contaCliente.saldo)){
+        p.textContent = `Saldo insuficiente. Saldo atual : ${contaCliente.saldo}`;
+        console.log("saldo invalido")
+        return; 
+    }
+
+    const novoSaldo = contaCliente.saldo - valor ;
+    atualizaSaldo(contaCliente, novoSaldo)
+    p.textContent = `Saque realizado com sucesso! Seu saldo disponível agora é de ${novoSaldo}`;
+    console.log(typeof valor)
+}
+
+sacar(contaCliente[1], 5)
