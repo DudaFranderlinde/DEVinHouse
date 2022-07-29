444e08f284f9d6f928d26ed21a1162ac472be729
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-07-29 17:04:59 -0300

    Exercício 3

--- semana5/index.js
+++ semana5/index.js
@@ -4,3 +4,17 @@ console.log(saudacao());
 
 import saudacaoEspecial from './saudacao-especial.js';
 console.log(saudacaoEspecial("Eduarda"));
+
+class Produto{
+    nome;
+    preco;
+    emEstoque;
+    quantidade;
+
+    constructor(nome, preco, emEstoque, quantidade){
+        this.nome = nome;
+        this.preco = preco;
+        this.emEstoque = emEstoque;
+        this.quantidade = quantidade;
+    }
+}
