a313a4010ed19f18e05aecbd0ee9ff1dc65af5bf
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-07-29 18:00:39 -0300

    Correção index.js

--- semana5/index.js
+++ semana5/index.js
@@ -5,32 +5,6 @@ console.log(saudacao());
 import saudacaoEspecial from './saudacao-especial.js';
 console.log(saudacaoEspecial("Eduarda"));
 
-class Produto{
-    nome;
-    preco;
-    emEstoque;
-    quantidade;
 
-    constructor(nome, preco, emEstoque, quantidade){
-        this.nome = nome;
-        this.preco = preco;
-        this.emEstoque = emEstoque;
-        this.quantidade = quantidade;
-    }
-}
 
-class Pedido{
-    numeroPedido;
-    dataPedido;
-    estaPago;
-    listaProdutos;
-    nomeCliente;
 
-    constructor(numPedido, nomeCliente){
-        this.numeroPedido = numPedido;
-        this.dataPedido = new Date().toLocaleDateString();
-        this.estaPago = false;
-        this.listaProdutos = [];
-        this.nomeCliente = nomeCliente;
-    }
-}
