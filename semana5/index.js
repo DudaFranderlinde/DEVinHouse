24f004c754ad951df688edeb94c9e1986a0e6ebb
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-07-29 17:23:37 -0300

    Exercício 4

--- semana5/index.js
+++ semana5/index.js
@@ -18,3 +18,19 @@ class Produto{
         this.quantidade = quantidade;
     }
 }
+
+class Pedido{
+    numeroPedido;
+    dataPedido;
+    estaPago;
+    listaProdutos;
+    nomeCliente;
+
+    constructor(numPedido, nomeCliente){
+        this.numeroPedido = numPedido;
+        this.dataPedido = new Date().toLocaleDateString();
+        this.estaPago = false;
+        this.listaProdutos = [];
+        this.nomeCliente = nomeCliente;
+    }
+}
