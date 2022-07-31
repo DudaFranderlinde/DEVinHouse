da0440dd05ed06785a32704f10dcdc7bdc502fb6
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-07-30 21:22:33 -0300

    Exercício 10

--- semana5/exercicos-9-10/index.js
+++ semana5/exercicos-9-10/index.js
@@ -8,3 +8,39 @@ function sleep ( valor ) {
   }
 
   sleep(4)
+
+  
+  function verificaHora(){
+    const hora = new Date();
+
+    if (typeof hora === 'object' && hora !== null && 'getHours' in hora) {
+        const result = hora.getHours();
+        return result
+    }
+  }
+
+  function verificaMin(){
+    const min = new Date();
+
+    if (typeof min === 'object' && min !== null && 'getMinutes' in min) {
+        const res = min.getMinutes();
+        return res
+    }
+}
+function verificaSeg(){
+    const seg = new Date();
+
+    if (typeof seg === 'object' && seg !== null && 'getSeconds' in seg) {
+        const resultado = seg.getSeconds();
+        return resultado
+    }
+}
+
+function horarioAtual(){
+    const hora = verificaHora();  
+    const min = verificaMin(); 
+    const seg = verificaSeg(); 
+    return console.log(`${hora}:${min}:${seg}`);
+}
+
+setInterval(horarioAtual, 2000)
