5e5843a4fe64451e06825aaf4bab438b998da517
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-08-21 20:33:16 -0300

    Exercício 5

--- semana8/src/components/ListaCards/ListaCards.jsx
+++ semana8/src/components/ListaCards/ListaCards.jsx
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import { Card } from '@components';
 
 import styles from './ListaCards.module.css';
+import { useState } from 'react';
 
-export const ListaCards = ({ produtos }) => (
+export const ListaCards = ({ produtos }) => {
+  const [ids, SetIds] = useState([])
+  return(
   <ul className={styles.lista}>
     {produtos.map((produto) => (
       <li className={styles.listaItem} key={produto.id}>
@@ -12,7 +15,7 @@ export const ListaCards = ({ produtos }) => (
       </li>
     ))}
   </ul>
-);
+)};
 
 ListaCards.propTypes = {
   produtos: PropTypes.array.isRequired,
