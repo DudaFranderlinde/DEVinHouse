09a5a24caf806b0afcf2924a96e82befe5b64db8
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-09-06 17:52:05 -0300

    Exercício 4

--- semana10/DEVInKnowledge/src/components/MainContent/MainCotent.jsx
+++ semana10/DEVInKnowledge/src/components/MainContent/MainCotent.jsx
@@ -1,42 +1,19 @@
+import { Summary } from '../Summary/Summary'
 import style from './MainContent.module.css'
 export const MainContent = () => {
+    const arrayL= [{titulo: "Total", val: 1},{titulo: "Back", val: 3},{titulo: "Front", val: 3},{titulo: "Full", val: 7}, {titulo: "Skill", val: 1}];
+
     return (
         <div className={style.main} >
             <main className={style.main} >
-                <header>
-                    <div className={style.cardsuperior}>
-                        <h3>Total</h3>
-                        <p id="total"></p>
-                    </div>
-            
-                    <div className={style.cardsuperior}>
-                        <h3>FrontEnd</h3>
-                        <p id="front"></p>
-                    </div>
-                    
-                    <div className={style.cardsuperior}>
-                        <h3>BackEnd</h3>
-                        <p id="back"></p>
-                    </div>
-            
-                    <div className={style.cardsuperior}>
-                        <h3>FullStack</h3>
-                        <p id="full"></p>
-                    </div>
-            
-                    <div className={style.cardsuperior}>
-                        <h3>SoftSkill</h3>
-                        <p id="soft"></p>
-                    </div>
-            
-                </header>
+                <Summary />
 
-                <div id="conatiner-barra-pesquisa" className={style.conatinerbarrapesquisa}>
+                <div id="conatinerbarrapesquisa" className={style.conatinerbarrapesquisa}>
                     <div id="div-busca">
                         <img src="src/assets/img/lupa.png" id="btnBusca" className={style.btnBusca}/>
                     </div>
                     <input type="text" id="textBusca" placeholder="Busque por um título"/>
-                    <div id="div-busca">
+                    <div id="divbusca">
                         <img src="src/assets/img/limpa.png" id="btnLimpaBarra" className={style.btnLimpaBarra}/>
                     </div>
                 </div>
