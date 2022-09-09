e5ab9aeb25c4507a4fbd0156332e1c0782e590ac
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-09-09 11:33:57 -0300

    Exercício 5

--- semana10/DEVInKnowledge/src/components/MainContent/MainCotent.jsx
+++ semana10/DEVInKnowledge/src/components/MainContent/MainCotent.jsx
@@ -1,5 +1,8 @@
+import { CardList } from '../CardList/CardList';
 import { Filter } from '../Filter/Filter';
 import { Summary } from '../Summary/Summary'
+import { TipCard } from '../TipCard/TipCard'
+
 import style from './MainContent.module.css'
 export const MainContent = () => {
     const arrayL= [{titulo: "Total", val: 1},{titulo: "Back", val: 3},{titulo: "Front", val: 3},{titulo: "Full", val: 7}, {titulo: "Skill", val: 1}];
@@ -10,10 +13,12 @@ export const MainContent = () => {
                 <Summary />
 
                <Filter/>
-                <div id="agrupaCard">
-                    
-                </div>
-                        
+               
+                <TipCard titulo={"HTML5"} descricao={"Duda é a melhor"} ></TipCard>
+
+                <CardList> 
+                    <TipCard/> 
+                </CardList>       
             </main>
         </div>
     )
--- semana10/DEVInKnowledge/src/components/TipCard/TipCard.jsx
+++ semana10/DEVInKnowledge/src/components/TipCard/TipCard.jsx
@@ -1,26 +1,28 @@
-
-export const TipCard = (titulo, descricao, categoria, linguagem, video)=>{
+import style from './TipCard.module.css'
+export const TipCard = ({titulo, descricao, categoria, linguagem, video})=>{
     return(
-        <>
+        <section className={style.sectionCard}>
             
-            <h1></h1>
+            <h1>{titulo}</h1>
             <ul>
                 <li>Linguagem: {linguagem} </li>
                 <li>Categoria: {categoria} </li>
                 <li><p>{descricao}</p></li>
             </ul>
-            <div>
-                <button>
-                    <img src="" alt="Vídeo" />
+            <div className={style.divImg}>
+                <button className={style.buttonCard}>
+                    <a href={video}>
+                    <img src="src/assets/img/video.png" alt="Vídeo" />
+                    </a>
                 </button>
-                <button>
-                    <img src="" alt="Deletar" />
+                <button className={style.buttonCard}>
+                    <img src="src/assets/img/delete.png" alt="Deletar" />
                 </button>
-                <button>
-                    <img src="" alt="Editar" />
+                <button className={style.buttonCard}>
+                    <img src="src/assets/img/edit.png" alt="Editar" />
                 </button>
             </div>
         
-        </>
+        </section>
     )
 }
