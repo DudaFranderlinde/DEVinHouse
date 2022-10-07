9a8e600291bd5a21d113a4f90fa5b41ed722733d
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-10-07 19:22:10 -0300

    Exercício 2

--- Modulo_2/Semana_2/main.js
+++ Modulo_2/Semana_2/main.js
@@ -1,7 +1,32 @@
 const express = require('express')
-const app = express()
+const { v4: uuidv4 } = require('uuid');
 
+const app = express()
 app.use(express.json())
+
+let saboresPizza = []
+
+app.get('/pizzas/:name', (request, response)=> {
+    response.status(200).json(saboresPizza)
+})
+
+app.post('/pizzas', (request, response)=> {
+    const novaPizza = {
+        id: uuidv4(),
+        name: request.body.name,
+        description: request.body.description,
+        price: request.body.price,
+        ingredients: request.body.ingredients,
+    }
+
+    saboresPizza.push(novaPizza);
+    response.status(201).json(novaPizza);
+})
+
+let pedidos = []
+app.get('/solicitations', (request, response)=> {
+    response.status(200).json(pedidos);
+})
 
 app.listen(3333, ()=> {
     console.log('Servidor Online');
