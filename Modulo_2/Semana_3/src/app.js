f51d874526deb0d2e1672cd76f63e9ce8be2887d
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-10-16 20:56:41 -0300

    Exercício 2

--- Modulo_2/Semana_3/src/app.js
+++ Modulo_2/Semana_3/src/app.js
@@ -1,6 +1,4 @@
 import express from 'express'
-import cors from 'cors'
-import  { v4 as uuidv4 } from 'uuid';
 import pizzaRoutes from './routes/pizzas.routes.js';
 import solicitationsRoutes from './routes/solicitations.routes.js';
 
@@ -9,4 +7,4 @@ app.use(express.json())
 app.use(pizzaRoutes)
 app.use(solicitationsRoutes)
 
-export default app
+export default app;
--- Modulo_2/Semana_3/src/controllers/pizzas.controllers.js
+++ Modulo_2/Semana_3/src/controllers/pizzas.controllers.js
@@ -1,16 +1,23 @@
 import { v4 as uuidv4 } from "uuid"
+import fs from 'fs'
+import { getPizzasFile } from "../utils/getPizzasFile.js"
+
 let saboresPizza = [] 
 
 export function listaPizzas(request, response) {
-    const pesquisaPizza = saboresPizza.filter(elemento=> elemento.name.toLowerCase().includes(request.query.name.toLocaleLowerCase()))
+   const listaPizzas = getPizzasFile()
+
+    const pesquisaPizza = listaPizzas.filter(elemento=> elemento.name.toLowerCase().includes(request.query.name.toLocaleLowerCase()))
     if (request.query.name) {
        return response.json(pesquisaPizza)
     }
-    response.status(200).json(saboresPizza)
+    response.status(200).json(listaPizzas)
 }
 
 export function criarPizza(request, response) {
-    const pizzaExiste = saboresPizza.find(elemento=> elemento.name === request.body.name);
+   const listaPizzas = getPizzasFile()
+
+    const pizzaExiste = listaPizzas.find(elemento=> elemento.name.toLocaleLowerCase() === request.body.name.toLocaleLowerCase());
 
     if (pizzaExiste) {
         return response.status(401).json({error: "Esta pizza já foi cadastrada"})
@@ -23,18 +30,20 @@ export function criarPizza(request, response) {
         ingredients: request.body.ingredients,
     }
 
-    saboresPizza.push(novaPizza);
+    fs.writeFileSync('pizzas.json', JSON.stringify([...listaPizzas, novaPizza]))
     response.status(201).json(novaPizza);
 }
 
 export function atualizandoPizza(request, response) {
-    const pizzaBusca = saboresPizza.find(elemento=> elemento.id === request.params.id);
+   let listaPizzas = getPizzasFile()
+
+    const pizzaBusca = listaPizzas.find(elemento=> elemento.id === request.params.id);
 
     if (!pizzaBusca) {
         return response.status(404).json({error: "Item não encontrado no sistema"})
     }
 
-    const atualizaPizza = saboresPizza.map(elemento=> {
+    const atualizaPizza = listaPizzas.map(elemento=> {
         if (elemento.id === request.params.id) {
             elemento.name = request.body.name,
             elemento.description = request.body.description,
@@ -44,12 +53,14 @@ export function atualizandoPizza(request, response) {
         return elemento;
     })
 
-    saboresPizza = [...atualizaPizza]
-    response.json(saboresPizza)
+    listaPizzas = [...atualizaPizza]
+    fs.writeFileSync('pizzas.json', JSON.stringify([...atualizaPizza]))
+    response.json(listaPizzas)
 }
 
 export function deletarPizza(request, response) {
-    const filtraPizzas = saboresPizza.filter(elemento => elemento.id !== request.params.id)
+    let listaPizzas = getPizzasFile()
+    const filtraPizzas = listaPizzas.filter(elemento => elemento.id !== request.params.id)
     tasks = [...filtraPizzas]
     response.json()
 }
