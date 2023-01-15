95f974d20d432f0f883ecfcc3b8ff3494f144a2f
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2023-01-15 17:21:05 -0300

    Exercício 5

--- Modulo_2/Semana_9/e_commerce/src/app.module.ts
+++ Modulo_2/Semana_9/e_commerce/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CarrinhoModule } from './carrinho/carrinho.module';
 import { databaseProviders } from './core/database/database.providers';
 import { ProdutosModule } from './produtos/produtos.module';
 import { produtosProviders } from './produtos/produtos.providers';
@@ -7,7 +8,7 @@ import { produtosProviders } from './produtos/produtos.providers';
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    ProdutosModule
+    ProdutosModule, CarrinhoModule
   ],
   controllers: [],
   providers: [...databaseProviders, ...produtosProviders],
