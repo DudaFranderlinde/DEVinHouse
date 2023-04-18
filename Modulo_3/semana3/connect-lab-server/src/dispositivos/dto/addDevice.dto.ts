15a11a4b4a616e3a8df60300b44b768c790d109d
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2023-04-18 03:06:47 -0300

    Exercício 4

--- Modulo_3/semana3/connect-lab-server/src/dispositivos/dto/addDevice.dto.ts
+++ Modulo_3/semana3/connect-lab-server/src/dispositivos/dto/addDevice.dto.ts
@@ -1,11 +1,16 @@
 import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { DispositivosDocumentation } from "../documentation";
+import { ApiProperty } from "@nestjs/swagger";
 
+const {ApiProperty:apiPro} = DispositivosDocumentation 
 export class AddDeviceDto{
     @IsNotEmpty()
     @IsNumber()
+    @ApiProperty(apiPro.addDevice.idDevice)
     idDevice: number;
 
     @IsNotEmpty()
     @IsString()
+    @ApiProperty(apiPro.addDevice.local)
     local: string;
 }
