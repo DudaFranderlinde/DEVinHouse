b068c81a662aeab5ef75c9878c8d079cf522b7f5
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2023-04-18 01:49:13 -0300

    Ex 1 - Setup inicial

--- Modulo_3/semana3/connect-lab-server/src/main.ts
+++ Modulo_3/semana3/connect-lab-server/src/main.ts
@@ -1,11 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupApiDocs } from './common/api-docs';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  setupApiDocs(app);
   await app.listen(3000);
   
 }
