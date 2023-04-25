ec09d40e232bdf7f6ca0467b87aab77e34f270d6
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2023-04-25 04:39:56 -0300

    Exercício 1

--- Modulo_3/semana4/api-developers/src/modules/cities/controllers/city.controller.ts
+++ Modulo_3/semana4/api-developers/src/modules/cities/controllers/city.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Post, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Param,
+  HttpException,
+  HttpStatus,
+  Body,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { StateService } from '../../states/services/state.service';
 import { CityService } from '../services/city.service';
 import axios from 'axios';
 import { City } from '../interfaces';
 import { ApiTags } from '@nestjs/swagger';
 import { CityEntity } from '../entities/city.entity';
+import { UpdateResult } from 'typeorm';
+import { CreateCityDto } from '../dto/create-city.dto';
 
 @ApiTags('cities')
 @Controller('city')
@@ -40,6 +52,45 @@ export class CityController {
         this.cityService.createCity(newCity);
       });
       return 'Cidades salvas com sucesso';
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  @Delete(':id')
+  async deleteCity(@Param('id') id: number): Promise<string> {
+    try {
+      if (!id) {
+        throw new HttpException(
+          { reason: 'Id inválido' },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      return await this.cityService.deleteCity(id);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  @Patch('updateCity/:id')
+  async updateCity(
+    @Param('id') id: number,
+    @Body() updateCity: CreateCityDto,
+  ): Promise<UpdateResult> {
+    try {
+      if (!id) {
+        throw new HttpException(
+          { reason: 'Alguma informação está incorreta, verifique novamente!' },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      if (!(await this.cityService.findById(id))) {
+        throw new HttpException(
+          { reason: 'Não encontramos uma cidade com este Id' },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return await this.cityService.updateCity(id, updateCity);
     } catch (error) {
       console.log(error);
     }
---
+++ Modulo_3/semana4/api-developers/src/modules/cities/services/city.service.ts
@@ -0,0 +1,60 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateCityDto } from '../dto/create-city.dto';
+import { CityRepository } from '../city.repository';
+import { CityEntity } from '../entities/city.entity';
+import { UpdateResult } from 'typeorm';
+
+@Injectable()
+export class CityService {
+  constructor(private readonly cityRepository: CityRepository) {}
+
+  async findById(id: number): Promise<CityEntity> {
+    const foundCity = await this.cityRepository.getById(id);
+    if (!foundCity) {
+      throw new NotFoundException('cityNotFound');
+    }
+
+    return foundCity;
+  }
+
+  async createCity(newCity: CreateCityDto): Promise<void> {
+    if (await this.findById(newCity.state_id)) {
+      throw new ConflictException('Essa cidade já está cadastrada!');
+    }
+    await this.cityRepository.createCity(newCity);
+  }
+
+  async deleteCity(id: number): Promise<string> {
+    const foundCity = await this.cityRepository.getById(id);
+    if (!foundCity) {
+      throw new NotFoundException(
+        'Não foi encontrada nenhuma cidade com este Id',
+      );
+    }
+
+    await this.cityRepository.deleteCity(id);
+
+    return 'Cidade excluída com sucesso!';
+  }
+
+  async updateCity(
+    cityId: number,
+    updateCityDTO: CreateCityDto,
+  ): Promise<UpdateResult> {
+    const updatedCity = await this.cityRepository.updateCity(
+      cityId,
+      updateCityDTO.name,
+      updateCityDTO.state_id,
+    );
+
+    if (!updatedCity) {
+      throw new NotFoundException('Não achamos uma cidade com este Id!');
+    }
+
+    return updatedCity;
+  }
+}
