b079dd3b5397190479fcccdb98634d1b55d7fbb1
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2023-04-25 05:34:39 -0300

    Exercício 3

---
+++ Modulo_3/semana4/api-developers/src/modules/cities/services/city.service.spec.ts
@@ -0,0 +1,41 @@
+import { TestStatic } from "src/utils/test";
+import { CityService } from "./city.service";
+
+describe('CountryController', () => {
+  let cityService: CityService;
+  
+  const mockService = {
+    findById: jest.fn(),
+    createCity: jest.fn(),
+    updateCity: jest.fn(),
+    deleteCity: jest.fn(),
+  };
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CityController],
+      providers: [{ provide: CityService, useValue: mockService }],
+    }).compile();
+
+    cityService = module.get<CityService>(CityService);
+  });
+
+  beforeEach(() => {
+    mockService.createCity.mockReset();
+    mockService.deleteCity.mockReset();
+    mockService.findById.mockReset();
+    mockService.updateCity.mockReset();
+  });
+  
+  describe('findById', () => {
+    it('deveria retornar o resultado da busca e devolver um registro de dados de país', async () => {
+      const city = TestStatic.countryData();
+      mockService.findById.mockReturnValue(city);
+      const foundCity = await cityService.findById(city.id)
+      expect(foundCity).toMatchObject({ id: city.id });
+      expect(mockService.findById).toHaveBeenCalledTimes(1);
+    });
+    })
+  
+  
+  }) 
