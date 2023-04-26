fbe932551189a663b9fea1e516c0d83507c661df
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2023-04-26 06:59:54 -0300

    Exercício 6

--- Modulo_3/semana5/lab_sushi_frontend/src/pages/Order/order.spec.js
+++ Modulo_3/semana5/lab_sushi_frontend/src/pages/Order/order.spec.js
@@ -1,72 +1,48 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { BrowserRouter } from "react-router-dom";
-import Order from ".";
-import { orderActions } from "../../actions/order.action";
-import { OPTIONS_PAYMENT } from "../../constants";
-import { CartContext } from "../../contexts/CartContext";
+import { render, screen } from '@testing-library/react';
+import { Order } from '.';
 
-const renderWithContext = (addProduct) => {
-  return render(
-    <CartContext.Provider value={{ addProduct: addProduct || jest.fn() }}>
-      <Order />
-    </CartContext.Provider>
-    , {
-      wrapper: BrowserRouter
-    }
-  );
-};
-
-describe('Order page', () => {
-
-  const interactInput = (name, value) => {
-    const input = screen.getByRole('textbox', { name });
-    fireEvent.change(input, { target: { value } });
-    expect(input.value).toBe(value)
-  }
-
-  it('Deve preencher corretamente o formulário', () => {
-
-    const addAction = jest.spyOn(orderActions, 'addOrderAction')
-
-    renderWithContext()
-
-    interactInput('name', 'Henrique')
-    interactInput('phone', '85991811574')
-    interactInput('cep', '62870000')
-    interactInput('address', 'Rua teste')
-    interactInput('neighborhood', 'Buriti')
-    interactInput('state', 'CE')
-    interactInput('city', 'Pacajus')
-
-    const paymentTypeSelect = screen.getByTestId('payment-type-select')
-
-    let options = screen.getAllByTestId('option-payment-type-select')
-
-    fireEvent.change(paymentTypeSelect, { target: { value: OPTIONS_PAYMENT[1].value } })
-
-    expect(options[0].selected).toBeFalsy();
-    expect(options[1].selected).toBeTruthy();
-    expect(options[2].selected).toBeFalsy();
-
-    userEvent.click(screen.getByTestId('submit-button-form'));
-
-    expect(addAction).toHaveBeenCalled();
-    expect(addAction).toBeCalledTimes(1);
-    expect(addAction).toBeCalledWith({
-      "clientAddress":
+describe('Order', () => {
+  it('renders the order cards correctly', async () => {
+    const mockOrders = [
       {
-        "address": "Rua teste",
-        "cep": "62870000",
-        "city": "Pacajus",
-        "neighborhood": "Buriti",
-        "state": "CE"
+        id: 1,
+        date: '2022-03-01',
+        address: '123 Main St',
+        state: 'Shipped',
+        hour: '14:00',
+        payment: 'Credit Card'
       },
-      "clientContact": "85991811574",
-      "clientName": "Henrique",
-      "haveHashi": "SIM",
-      "paymentType": "CRED"
-    })
-  });
+      {
+        id: 2,
+        date: '2022-03-02',
+        address: '456 Oak St',
+        state: 'Delivered',
+        hour: '12:30',
+        payment: 'PayPal'
+      }
+    ];
 
-})
+    jest.spyOn(global, 'fetch').mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockOrders)
+      })
+    );
+
+    render(<Order/>);
+
+    const orderCards = await screen.findAllByTestId('order-card');
+
+    expect(orderCards).toHaveLength(mockOrders.length);
+
+    orderCards.forEach((orderCard, index) => {
+      expect(orderCard).toHaveTextContent(`#${mockOrders[index].id}`);
+      expect(orderCard).toHaveTextContent(mockOrders[index].date);
+      expect(orderCard).toHaveTextContent(mockOrders[index].address);
+      expect(orderCard).toHaveTextContent(mockOrders[index].state);
+      expect(orderCard).toHaveTextContent(mockOrders[index].hour);
+      expect(orderCard).toHaveTextContent(mockOrders[index].payment);
+    });
+
+    global.fetch.mockRestore();
+  });
+});
