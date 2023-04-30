3619ee957912e2025cd98e2ec71d96575facc233
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2023-04-30 20:25:33 -0300

    Finalizando

--- Modulo_3/semana9/exercicio8.go
+++ Modulo_3/semana9/exercicio8.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"labmath"
+	"semana9/labmath/labmath"
 )
 
 
---
+++ Modulo_3/semana9/labmath/labmath.go
@@ -0,0 +1,8 @@
+package labmath
+
+func IsPrimo(num int) bool{
+	if num%2 == 0 {
+		return false;
+	};
+	return true
+}
