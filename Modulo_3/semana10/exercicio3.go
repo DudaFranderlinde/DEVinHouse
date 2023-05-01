8d794140158e4625c275085741e624e9b09a3b67
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2023-04-30 22:12:48 -0300

    Exercício 3

---
+++ Modulo_3/semana10/exercicio3.go
@@ -0,0 +1,31 @@
+package main
+
+func main() {
+	
+}
+
+type Veiculo struct {
+	Marca  string
+	Modelo string
+}
+
+type Carro struct {
+	Veiculo
+	numeroDePortas int
+}
+
+type Moto struct {
+	Veiculo
+	cilindradas int
+}
+
+type Revisao interface {
+	FazerRevisao(carro Carro, moto Moto)
+}
+
+func agendarRevisao(r Revisao) {
+	carro := Carro{Veiculo{"Honda", "City"}, 4}
+	moto := Moto{Veiculo{"Honda", "Biz"}, 124}
+
+	r.FazerRevisao(carro, moto)
+}
